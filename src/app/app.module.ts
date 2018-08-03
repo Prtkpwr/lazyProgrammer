a6b46b93f1427c53c3eef2770f4ce51aa6fc6198
prtkpwr <pratik010pawar@gmail.com>
2018-08-04 03:00:19 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { HomeTimeComponent } from './home-time/home-time.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeTimeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'gohome', pathMatch:'full'},
+      { path: 'gohome', component: HomeTimeComponent },
+      {path:'**',redirectTo:'gohome', pathMatch:'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/home-time/home-time.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-time',
+  templateUrl: './home-time.component.html',
+  styleUrls: ['./home-time.component.css']
+})
+export class HomeTimeComponent implements OnInit {
+  public inTimeH:number = 0;
+  public inTimeM:number = 0;
+  public workTimeH:number = 0;
+  public workTimeM:number = 0;
+  public outTimeH:number = 0;
+  public outTimeM:number = 0;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  public countIt(){
+    this.outTimeH = this.inTimeH + this.workTimeH;
+    this.outTimeM = this.inTimeM + this.workTimeM;
+    if( this.outTimeM == 60 ){
+      this.outTimeH ++;
+      this.outTimeM = 0;
+          }
+      else if (this.outTimeM > 60){
+	    this.outTimeH ++;
+      this.outTimeM = this.outTimeM - 60
+          }
+     
+      }
+}
